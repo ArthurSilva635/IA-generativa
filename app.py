5ceacc03eb8f1b80a1aa51b76c657ac96bc33155
027452511643 <arthur.goncalves.estagiario@gmail.com>
2025-02-25 19:26:08 -0300

    Inserindo as mensagens do usuário dentro da interface, com histórico

--- app.py
+++ app.py
@@ -5,19 +5,27 @@ from streamlit_chat import message
 
 def main():
 
-    st.set_page_config(page_title='Chat PDF TRE-RN', page_icon=':books:')
+    st.set_page_config(
+        page_title='Chat PDF TRE-RN', 
+        page_icon=':books:'
+    )
 
-    st.header('Converse com seus arquivos')
+    st.header('Chat PDF TRE-RN - Sentenças')
     user_question = st.text_input("Faça uma pergunta para mim!")
 
-    if('conversation' not in st.session_state):
+    if('conversation' not in st.session_state): # Armazen as variáveis dentro de um estado de sessão
         st.session_state.conversation = None
 
+    # Inserindo as mensagens do usario dentro da nossa interface
     if(user_question):
-        response = st.session_state.conversation(user_question) ['chat_history'][-1]
+        response = st.session_state.conversation(user_question) ['chat_history']
 
-        message(user_question, is_user=True)
-        message(response.content, is_user=False)
+        for i, text_message in enumerate(response):
+
+            if(i % 2 == 0):
+                message(text_message.content, is_user=True, key=str(i) + '_user')
+            else:
+                message(text_message.content, is_user=False, key=str(i) + '_bot')
 
 
     with st.sidebar:
